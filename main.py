c24e744b2ebb22c9a9c84eb2300cc53a70e858b4
Renan Barbosa <renansantosmath@gmail.com>
2022-02-25 11:25:53 -0300

    mini projeto finalizado

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from time import sleep
 
 
 def main():
@@ -18,22 +19,33 @@ def main():
     cep_data = consumo_api.json()
 
     # Organização das informações 72318526
+    sleep(0.5)
     print('=' * 42)
     if 'erro' not in cep_data.keys():
-        print(f'CEP consultado: {cep_data["cep"]}\n'
-              f'Endereço: {cep_data["logradouro"]} {cep_data["complemento"]}\n'
-              f'Localidade/UF: {cep_data["localidade"]}-{cep_data["uf"]}')
+        sleep(0.5)
+        print(f'CEP consultado: {cep_data["cep"]}')
+        sleep(0.5)
+        print(f'Endereço: {cep_data["logradouro"]} {cep_data["complemento"]}')
+        sleep(0.5)
+        print(f'Localidade/UF: {cep_data["localidade"]}-{cep_data["uf"]}')
+        sleep(2)
     else:
         print(f'\033[31mCEP {cep_option} inválido!\033[m')
     print('=' * 42)
+    sleep(0.5)
     user_option = input('Deseja consultar outro CEP [S/N]? ').upper()
     while user_option not in 'SN':
         print(f'\033[31mErro! {user_option} é inválido!')
         user_option = input('Deseja consultar outro CEP [S/N]? ').upper()
     if user_option.upper() == 'S':
+        print('\n' * 100)
         main()
     else:
-        print('\033[31mEncerrando o programa...')
+        print('\033[31mEncerrando o programa\033[m', end='')
+        for c in range(3):
+            sleep(0.5)
+            print('\033[31m.\033[m', end='')
+        print()
 
 
 if __name__ == '__main__':
